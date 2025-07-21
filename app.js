5cd51e8610827388c84cd248c7dde5a023624522
Ziad Morjan <zmorjan765@gmail.com>
2025-07-21 13:27:38 +0300

    chore: update dependencies and add security packages
    - Added express-mongo-sanitize and helmet for enhanced security.
    - Updated multer to version 2.0.2 for improved functionality.
    - Added toobusy-js and xss-clean for performance and security improvements.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const passport = require('passport');
+const toobusy = require('toobusy-js');
+const expressLimit = require("express-rate-limit");
+const hpp = require('hpp');
+const helmet = require("helmet");
+const sanitizer = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 // import strategy
 const strategies = require('./config/passport');
@@ -10,6 +16,7 @@ const routes = require("./routes");
 
 // import globalErrorHandler
 const { globalErrorHandler } = require("./middlewares/ErrorMiddleware");
+const CustomError = require("./utils/CustomError");
 
 let app = express();
 
@@ -17,13 +24,35 @@ let app = express();
 passport.use(strategies.googleStrategy);
 passport.use(strategies.facebookStrategy);
 
+//webhook route
 const { webhook } = require("./controllers/PaymentController");
 app.post("/api/v1/webhook", express.raw({ type: "application/json" }), webhook);
 
+// rate limiter
+const authLimiter = expressLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later."
+});
+
 // middlewares
+app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "1kb" }));
+app.use(express.json({ limit: "1kb" }));
+
+app.use(function (req, res, next) {
+    if (toobusy()) throw new CustomError("Server Too Busy", 503);
+    else next();
+});
+
+app.use(hpp());
+app.use(sanitizer());
+app.use(xss());
 app.use(express.static("uploads"));
+
+app.use("/api/v1/auth", authLimiter);
+
 app.use(passport.initialize());
 
 // routes
